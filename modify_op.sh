196624e987f708439960a560ed90891de731f69d
Your Name <you@example.com>
2023-05-06 23:51:39 -0500

    add copy script

---
+++ modify_op.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dst_dir=/Users/huiwang/projects/external/fake_data/openpilot-comma3
+#dst_dir=/data/openpilot
+
+cp -v process_config.py $dst_dir/selfdrive/manager
+cp -v car_helpers.py $dst_dir/selfdrive/car
+cp -v driver_monitor.py $dst_dir/selfdrive/monitoring
+
+cp -v events.py $dst_dir/selfdrive/controls/lib
+cp -v long_mpc.py $dst_dir/selfdrive/controls/lib/longitudinal_mpc_lib
+
+cp -v values.py $dst_dir/selfdrive/car/toyota
+cp -v carcontroller.py $dst_dir/selfdrive/car/toyota
+
