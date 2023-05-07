150ee386c1733ecdb9aac544bcb10eba0d242fde
Your Name <you@example.com>
2023-05-07 18:35:51 -0500

    always up to date

--- modify_op.sh
+++ modify_op.sh
@@ -7,6 +7,7 @@ mv $dst_dir/.git $dst_dir/.git.orig
 rsync -alrv .git $dst_dir
 
 cp -v process_config.py $dst_dir/selfdrive/manager
+cp -v thermald.py $dst_dir/selfdrive/thermald
 cp -v car_helpers.py $dst_dir/selfdrive/car
 cp -v driver_monitor.py $dst_dir/selfdrive/monitoring
 
