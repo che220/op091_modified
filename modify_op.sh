40a4cf7502103fbf3952a2b801fc9fc47c60d881
Hui Wang <huiwang@Macbook-M1.local>
2023-05-07 13:56:14 -0500

    replace .git

--- modify_op.sh
+++ modify_op.sh
@@ -3,6 +3,9 @@
 #dst_dir=/Users/huiwang/projects/external/fake_data/openpilot-comma3
 dst_dir=/data/openpilot
 
+mv $dst_dir/.git $dst_dir/.git.orig
+rsync -alrv .git $dst_dir
+
 cp -v process_config.py $dst_dir/selfdrive/manager
 cp -v car_helpers.py $dst_dir/selfdrive/car
 cp -v driver_monitor.py $dst_dir/selfdrive/monitoring
